4c1ad627fa9194e374bfc1ec10b988ddc477fec7
Faisal Bin Ahmed <faisalbin@outlook.com>
2021-07-07 18:01:10 +0200

    Update petData.js

--- data/petData.js
+++ data/petData.js
@@ -2,16 +2,81 @@ export const pets = [
 	{
 		id: "dp",
 		name: "Differential Privacy",
-		arr: [1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0],
+		arr: [1, 1, 1, 1, 2, 1, 0, 1, 1, 0, 1, 3, 2],
 	},
 	{
 		id: "fl",
 		name: "Federated Learning",
-		arr: [0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0],
+		arr: [3, 3, 1, 1, 1, 3, 1, 1, 1, 2, 1, 3, 2],
 	},
 	{
 		id: "zkp",
 		name: "Zero Knowledge proof",
-		arr: [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0],
+		arr: [3, 1, 3, 3, 1, 0, 3, 1, 1, 1, 3, 2, 2],
+	},
+	{
+		id: "he",
+		name: "Homomorphic encryption",
+		arr: [3, 2, 3, 1, 1, 3, 1, 3, 1, 1, 3, 2, 2],
+	},
+	{
+		id: "smc",
+		name: "Secure multiparty computation",
+		arr: [3, 3, 1, 2, 1, 3, 1, 1, 1, 1, 3, 2, 2],
+	},
+	{
+		id: "ob",
+		name: "Obfuscation",
+		arr: [3, 1, 2, 2, 2, 3, 1, 3, 2, 1, 1, 3, 2],
+	},
+	{
+		id: "dape",
+		name: "Data Perturbation",
+		arr: [1, 1, 2, 2, 2, 3, 0, 3, 2, 0, 3, 3, 2],
+	},
+	{
+		id: "anon",
+		name: "Anonymization",
+		arr: [1, 1, 3, 2, 2, 3, 2, 1, 1, 0, 1, 1, 3],
+	},
+	{
+		id: "kan",
+		name: "K-anonymity",
+		arr: [1, 1, 3, 2, 2, 3, 2, 1, 1, 0, 1, 1, 1],
+	},
+	{
+		id: "ld",
+		name: "L-diversity",
+		arr: [1, 1, 3, 2, 2, 3, 2, 1, 1, 0, 1, 1, 1],
+	},
+	{
+		id: "tc",
+		name: "T-closeness",
+		arr: [1, 1, 3, 2, 2, 3, 2, 1, 1, 0, 1, 1, 0],
+	},
+	{
+		id: "psu",
+		name: "Psuedonymization",
+		arr: [0, 1, 3, 2, 2, 3, 2, 1, 1, 0, 1, 1, 2],
+	},
+	{
+		id: "ran",
+		name: "Randomization",
+		arr: [1, 1, 3, 2, 2, 2, 3, 2, 2, 0, 1, 3, 2],
+	},
+	{
+		id: "clo",
+		name: "Data Cloacking",
+		arr: [1, 2, 3, 2, 2, 2, 3, 2, 2, 0, 1, 3, 2],
+	},
+	{
+		id: "mas",
+		name: "Data Masking",
+		arr: [1, 1, 3, 2, 2, 2, 3, 2, 2, 0, 1, 3, 2],
+	},
+	{
+		id: "syn",
+		name: "Synthetic Data Generation",
+		arr: [1, 3, 3, 2, 2, 1, 1, 2, 2, 0, 1, 1, 2],
 	},
 ];
